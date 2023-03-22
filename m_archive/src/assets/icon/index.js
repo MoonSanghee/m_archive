a1f97c7ce7d537a54c1580ef81e4c79f1d966497
jinjuseo <wjbbdq1223@knu.ac.kr>
2023-03-23 00:28:13 +0900

    feature/starRate,starRateButton

--- m_archive/src/assets/icon/index.js
+++ m_archive/src/assets/icon/index.js
@@ -9,3 +9,8 @@ export { ReactComponent as LogoutIcon } from "./logout-icon.svg";
 export { ReactComponent as FAQIcon } from "./FAQ-icon.svg";
 export {ReactComponent as IconCheck} from "./check.svg";
 export {ReactComponent as IconCheck2} from "./checkcheck.svg";
+export {ReactComponent as StarIconYellow } from "./star-yellow-icon.svg";
+export {ReactComponent as StarIconWhite } from "./star-white-icon.svg";
+export {ReactComponent as HalfStarIcon } from "./star-half-icon.svg";
+export {ReactComponent as StarIcon } from "./star-icon.svg";
+
