0ede2c045a94dd24b3830b651ad9341f25b40433
ImSinYeong <tardiswhoi@naver.com>
2023-03-23 19:08:45 +0900

    bugfix/button,tag

--- m_archive/src/components/Common/index.js
+++ m_archive/src/components/Common/index.js
@@ -10,3 +10,4 @@ export {default as CheckBox} from "./CheckBox";
 export {default as CheckBox2} from "./CheckCheck";
 export {default as Button} from "./Button";
 export {default as StarRate} from './StarRate';
+export {default as Tag} from './Tag';
