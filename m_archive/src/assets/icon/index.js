bee4030d119c85663cb9463c431b78cb444b7a27
BlankCode <110416295+BlankCodeStack@users.noreply.github.com>
2023-03-23 15:33:16 +0900

    orgin/feature/accordion

--- m_archive/src/assets/icon/index.js
+++ m_archive/src/assets/icon/index.js
@@ -9,4 +9,7 @@ export { ReactComponent as LogoutIcon } from "./logout-icon.svg";
 export { ReactComponent as FAQIcon } from "./FAQ-icon.svg";
 export {ReactComponent as IconCheck} from "./check.svg";
 export {ReactComponent as IconCheck2} from "./checkcheck.svg";
+
+
+
 export {ReactComponent as MypageArrow} from "./Mypage-arrow.svg";
