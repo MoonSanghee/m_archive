95f31f5affa7ab8f279ce4551b6d635ecaa89c40
jinjuseo <wjbbdq1223@knu.ac.kr>
2023-03-24 15:53:32 +0900

    bugfix/아이콘이름정리

--- m_archive/src/assets/icon/index.js
+++ m_archive/src/assets/icon/index.js
@@ -1,22 +1,33 @@
+//드랍다운-쉐브론아이콘
 export { ReactComponent as ChevronIcon } from "./chevron-down.svg";
 export { ReactComponent as ChevronArrow } from "./chevron-arrow.svg";
+//프로필 아이콘
 export { ReactComponent as ProfileIcon } from "./profile-icon.svg";
+//마이페이지 아이콘
 export { ReactComponent as MyPageIcon } from "./mypage-icon.svg";
-export { ReactComponent as LikeIcon } from "./like-icon.svg";
-export { ReactComponent as ReviewWriteIcon } from "./write-review-icon.svg";
 export { ReactComponent as SettingIcon } from "./setting-icon.svg";
 export { ReactComponent as LogoutIcon } from "./logout-icon.svg";
 export { ReactComponent as FAQIcon } from "./FAQ-icon.svg";
+//돋보기 아이콘
 export { ReactComponent as SearchIcon } from "./search-icon.svg";
+//체크박스아이콘
 export {ReactComponent as IconCheck} from "./check.svg";
 export {ReactComponent as IconCheck2} from "./checkcheck.svg";
+//별점 아이콘
 export {ReactComponent as HalfStarIcon } from "./star-half-icon.svg";
 export {ReactComponent as StarIcon } from "./star-icon.svg";
+//닫기
 export {ReactComponent as CloseIcon} from "./close-icon.svg";
+//리뷰작성,수정아이콘
 export {ReactComponent as ReviewWriteIcon} from "./review-write-icon.svg";
 export {ReactComponent as ReviewModifyIcon} from "./review-modify-icon.svg";
+//공유 아이콘
 export {ReactComponent as ShareIcon} from "./share-icon.svg";
+//아코디언 아이콘
 export {ReactComponent as MypageArrow} from "./Mypage-arrow.svg";
+//좋아요 아이콘
+export { ReactComponent as LikeIcon } from "./like-icon.svg";
+//Movie - Card
 export {ReactComponent as LikeRedIcon} from "./like-red.svg";
 export {ReactComponent as LikeBlackIcon} from "./like-black.svg";
 export {ReactComponent as BlockIcon} from "./block.svg";
