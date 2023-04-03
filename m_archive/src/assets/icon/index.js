14233e682a91b7f517ef4499a56daece3ba60ffb
ImSinYeong <tardiswhoi@naver.com>
2023-04-03 22:51:54 +0900

    feature/moviesSearch

--- m_archive/src/assets/icon/index.js
+++ m_archive/src/assets/icon/index.js
@@ -1,39 +1,40 @@
 //드랍다운-쉐브론아이콘
-export { ReactComponent as ChevronIcon } from "./chevron-down.svg";
-export { ReactComponent as ChevronArrow } from "./chevron-arrow.svg";
+export { ReactComponent as ChevronIcon } from './chevron-down.svg';
+export { ReactComponent as ChevronArrow } from './chevron-arrow.svg';
 //프로필 아이콘
-export { ReactComponent as ProfileIcon } from "./profile-icon.svg";
+export { ReactComponent as ProfileIcon } from './profile-icon.svg';
 //마이페이지 아이콘
-export { ReactComponent as MyPageIcon } from "./mypage-icon.svg";
-export { ReactComponent as SettingIcon } from "./setting-icon.svg";
-export { ReactComponent as LogoutIcon } from "./logout-icon.svg";
-export { ReactComponent as FAQIcon } from "./FAQ-icon.svg";
+export { ReactComponent as MyPageIcon } from './mypage-icon.svg';
+export { ReactComponent as SettingIcon } from './setting-icon.svg';
+export { ReactComponent as LogoutIcon } from './logout-icon.svg';
+export { ReactComponent as FAQIcon } from './FAQ-icon.svg';
 //돋보기 아이콘
-export { ReactComponent as SearchIcon } from "./search-icon.svg";
+export { ReactComponent as SearchIcon } from './search-icon.svg';
 //체크박스아이콘
-export {ReactComponent as IconCheck} from "./check.svg";
-export {ReactComponent as IconCheck2} from "./checkcheck.svg";
+export { ReactComponent as IconCheck } from './check.svg';
+export { ReactComponent as IconCheck2 } from './checkcheck.svg';
 //별점 아이콘
-export {ReactComponent as HalfStarIcon } from "./star-half-icon.svg";
-export {ReactComponent as StarIcon } from "./star-icon.svg";
+export { ReactComponent as HalfStarIcon } from './star-half-icon.svg';
+export { ReactComponent as StarIcon } from './star-icon.svg';
 //닫기
-export {ReactComponent as CloseIcon} from "./close-icon.svg";
+export { ReactComponent as CloseIcon } from './close-icon.svg';
 //리뷰작성,수정아이콘
-export {ReactComponent as ReviewWriteIcon} from "./review-write-icon.svg";
-export {ReactComponent as ReviewModifyIcon} from "./review-modify-icon.svg";
+export { ReactComponent as ReviewWriteIcon } from './review-write-icon.svg';
+export { ReactComponent as ReviewModifyIcon } from './review-modify-icon.svg';
 //공유 아이콘
-export {ReactComponent as ShareIcon} from "./share-icon.svg";
+export { ReactComponent as ShareIcon } from './share-icon.svg';
 //아코디언 아이콘
-export {ReactComponent as MypageArrow} from "./Mypage-arrow.svg";
+export { ReactComponent as MypageArrow } from './Mypage-arrow.svg';
 //좋아요 아이콘
-export { ReactComponent as LikeIcon } from "./like-icon.svg";
+export { ReactComponent as LikeIcon } from './like-icon.svg';
 //Movie - Card
-export {ReactComponent as LikeRedIcon} from "./like-red.svg";
-export {ReactComponent as LikeBlackIcon} from "./like-black.svg";
-export {ReactComponent as BlockIcon} from "./block.svg";
+export { ReactComponent as LikeRedIcon } from './like-red.svg';
+export { ReactComponent as LikeBlackIcon } from './like-black.svg';
+export { ReactComponent as BlockIcon } from './block.svg';
 //ReviewCardCarousel - slider 원 아이콘
-export {ReactComponent as EclipseIcon} from "./eclipse-icon.svg";
+export { ReactComponent as EclipseIcon } from './eclipse-icon.svg';
 
 //블라인드아이콘
-export {ReactComponent as EyeIcon} from "./eye-icon.svg";
-export {ReactComponent as EyeBlindIcon} from "./eye-blind-icon.svg";
+export { ReactComponent as EyeIcon } from './eye-icon.svg';
+export { ReactComponent as EyeBlindIcon } from './eye-blind-icon.svg';
+export { ReactComponent as WarningIcon } from './warning-mark.svg';
--- m_archive/src/pages/index.js
+++ m_archive/src/pages/index.js
@@ -1,5 +1,6 @@
 export * from './Test';
-export * from "./Auth";
-export {default as HomePage} from "./Home";
-export { default as MovieDetailPage } from "./Home/MovieDetail";
-export { default as ReviewsPage } from "./Home/MovieDetail/Reviews";
+export * from './Auth';
+export { default as HomePage } from './Home';
+export { default as MovieDetailPage } from './Home/MovieDetail';
+export { default as MovieSearchPage } from './Home/MovieSearch';
+export { default as ReviewsPage } from './Home/MovieDetail/Reviews';
