0c184071add7381884c760134fd67dc1f32d7069
BlankCode <110416295+BlankCodeStack@users.noreply.github.com>
2023-04-04 16:55:09 +0900

    feacture/mypage/profile 마이페이지  profile 와 FAQ 건드는중 중간과정.

--- m_archive/src/pages/Test/Kim2/index.js
+++ m_archive/src/pages/Test/Kim2/index.js
@@ -1,56 +1,52 @@
-import React from "react";
-import styles from "./kim.module.scss";
-import { Button, SearchBox, Tag } from "../../../components/Common";
-import Accordion from "../../../components/Common/Accordion";
+import React, { useState } from "react";
+import icon from "../../../assets/icon"
 
+// 프로필 아이콘 교체 기능 테스트 중 
+function Kim2({ src, alt }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newSrc, setNewSrc] = useState(src);
 
-const data = [
-  {
-    title: "선호 장르는 어디서 바꿀 수 있나요?",
-    content:
-      "마이페이지 - 프로필 에서 하단에 선호장르를 변경 후 저장하시면 변경 됩니다.",
-  },
-  {
-    title: "제가 쓴 리뷰는 어디서 볼수 있나요?",
-    content:
-      "마이페이지 - 리뷰&댓글 에서 자신이 쓴 리뷰 혹은 댓글 내용을 확인 하실 수 있습니다.",
-  },
-  {
-    title: "프로필 아이콘은 어디서 바꾸나요?",
-    content:
-      "마이페이지 - 프로필 에서 아이콘 버튼을 클릭 후 원하는 아이콘 혹은 이미지 를 첨부하여 변경 하실수 있습니다.",
-  },
-  {
-    title: "추가 문의사항은 어디로 문의 하나요?",
-    content:
-      "기타 문의사항은 마이페이지 - 고객센턴 - 문의하기 버튼을 이용하여 작성해주시면 빠른시일내에 답변을 도와드리겠습니다.",
-  },
-];
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
 
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
 
+  const handleIconChange = (newSrc) => {
+    setNewSrc(newSrc);
+    setIsModalOpen(false);
+  };
 
-const Kim2 = () =>{
+  return (
+    <div>
+      <img src={newSrc} alt={alt} onClick={handleModalOpen} />
 
- return (
-<main className={styles.mainContainer}>
-   <h1> FAQ</h1>
-   <div className={styles.mainInput}>
-   <SearchBox />
-   <div className={Styles.sideButton}>
-   <Button>문의 내역</Button>
-   <Button>문의 하기</Button>
-   </div>
-   </div>
-  <ul className={styles.accordionWrapper}>
-  {data.map((item, index) => (
-    <li key={index} style={{ marginBottom: "5px" }}>
-      <Accordion title={item.title} content={item.content} />
-    </li>
-  ))}
-</ul>
-
-</main>
- );
-    
+      {isModalOpen && (
+        <div>
+          <div>Choose a new profile icon:</div>
+          <div>
+            <img
+              src= "image.png"
+              alt="icon_1"
+              onClick={() => handleIconChange("url_to_icon_1")}
+            />
+            <img
+              src="url_to_icon_2"
+              alt="icon_2"
+              onClick={() => handleIconChange("url_to_icon_2")}
+            />
+            <img
+              src="url_to_icon_3"
+              alt="icon_3"
+              onClick={() => handleIconChange("url_to_icon_3")}
+            />
+          </div>
+          <button onClick={handleModalClose}>Cancel</button>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Kim2;
