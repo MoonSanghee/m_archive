dbaebb204dc37998ce3e281195c868705d16026f
kyoongdev <9898junjun2@gmail.com>
2023-04-10 17:27:11 +0900

    feedback : 2023.04.10

--- m_archive/src/components/Common/ShareButton/index.js
+++ m_archive/src/components/Common/ShareButton/index.js
@@ -1,37 +1,41 @@
-import React, { memo } from "react";
-import { ShareIcon } from "../../../assets/icon";
-import styles from "./sharebutton.module.scss";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import swal from "sweetalert2";
-import cx from "classnames";
-import { useLocation } from "react-router-dom";
+import React, { memo } from 'react';
+import { ShareIcon } from '../../../assets/icon';
+import styles from './sharebutton.module.scss';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import swal from 'sweetalert2';
+import cx from 'classnames';
+import { useLocation } from 'react-router-dom';
 
-const ShareButton = ({label,className,url}) => {
+const ShareButton = ({ label, className, url }) => {
   const location = useLocation();
+  //NOTE: host url도 같이 복사
   const currentUrl = url ? url : location.pathname;
-  
+
   const onShareBtnClick = () => {
     //NOTE: position 으로 조절하거나, showClass로 조절
     swal.fire({
-      text: "링크를 클립보드에 복사했습니다.",
-      position: "bottom-left",
+      text: '링크를 클립보드에 복사했습니다.',
+      position: 'bottom-left',
       showClass: {
         popup: styles.popup,
       },
       timer: 1000,
       showConfirmButton: false,
-      padding: "0",
-      width: 320
+      padding: '0',
+      width: 320,
     });
   };
 
   return (
-      <section className={cx(styles.wrapper,className)} onClick={onShareBtnClick}>
-        <CopyToClipboard text={currentUrl}>
-          <ShareIcon /> 
-        </CopyToClipboard>
-        {label && <span className={styles.label}>{label}</span>}
-      </section>
+    <section
+      className={cx(styles.wrapper, className)}
+      onClick={onShareBtnClick}
+    >
+      <CopyToClipboard text={currentUrl}>
+        <ShareIcon />
+      </CopyToClipboard>
+      {label && <span className={styles.label}>{label}</span>}
+    </section>
   );
 };
 
