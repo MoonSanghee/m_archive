f324ea3c8fbdd8b3085aab60d9d58d718293bb3d
jinjuseo <wjbbdq1223@knu.ac.kr>
2023-04-10 18:33:23 +0900

    bugfix/fqaModal

---
+++ m_archive/src/pages/MyPage/FAQ/FAQModal/index.js
@@ -0,0 +1,19 @@
+const FAQModal = ({type,onClose}) =>{
+    if(type==="questions"){
+        return(
+            
+            <section>
+
+            </section>
+        )
+    }
+    else{
+        return(
+            
+            <section>
+
+            </section>
+        )
+    }
+}
+export default FAQModal;
