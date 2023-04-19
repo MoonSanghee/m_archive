669d7a76f58746d31b3bdaca8e989b2770b12c22
BlankCode <110416295+BlankCodeStack@users.noreply.github.com>
2023-04-19 13:20:21 +0900

    bugfix/faq 작업중 문의내역 작업중

--- src/pages/MyPage/FAQ/FAQListModal/questionDetail.js
+++ src/pages/MyPage/FAQ/FAQListModal/questionDetail.js
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 import styles from "./faqListModal.module.scss";
 
 
-
-
 const QuestionDetail = ({ question }) => {
+
   const [answer, setAnswer] = useState('답변을 준비중입니다.')
-  if (question.faqComment) {
-    setAnswer(question.faqComment)
-  }
+  useEffect(() => {
+    if (question.faqComment && question.faqComment !== answer) {
+      setAnswer(question.faqComment)
+    }
+  }, [question, answer]);
+
   return (
     <div className={styles.questionDetail}>
       {/* <strong className={styles.questionTitle}>{question.title}</strong>
       <span className={styles.questionStatus}>{question.status}</span>*/}
       <div className={styles.questionContent}>{question.content}</div>
-    <div className={styles.answer}>{answer}{question.answer}</div>
+    <div className={styles.answer}>{question.answer}</div>
 </div>
   );
 };
