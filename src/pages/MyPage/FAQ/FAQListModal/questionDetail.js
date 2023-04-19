98f950a0c9b31431624c5fce43199be7f7495a1b
MoonSanghee <psycho13th@gmail.com>
2023-04-19 13:37:32 +0900

    답변 호출 에러 수정

--- src/pages/MyPage/FAQ/FAQListModal/questionDetail.js
+++ src/pages/MyPage/FAQ/FAQListModal/questionDetail.js
@@ -3,11 +3,11 @@ import styles from "./faqListModal.module.scss";
 
 
 const QuestionDetail = ({ question }) => {
-
+  console.log(question.faqComment)
   const [answer, setAnswer] = useState('답변을 준비중입니다.')
   useEffect(() => {
-    if (question.faqComment && question.faqComment !== answer) {
-      setAnswer(question.faqComment)
+    if (question.faqComment) {
+      setAnswer(question.faqComment.content)
     }
   }, [question, answer]);
 
@@ -16,8 +16,8 @@ const QuestionDetail = ({ question }) => {
       {/* <strong className={styles.questionTitle}>{question.title}</strong>
       <span className={styles.questionStatus}>{question.status}</span>*/}
       <div className={styles.questionContent}>{question.content}</div>
-    <div className={styles.answer}>{answer}{question.answer}</div>
-</div>
+      <div className={styles.answer}>-{answer}</div>
+    </div>
   );
 };
 
